710c1a50cc539548c6b89981f34b80314dad3051
BossIan <ianjoson29@gmail.com>
2021-05-17 14:34:57 +0800

    0.0.0.4 Movemnent.

--- src/scenes/testlevel.js
+++ src/scenes/testlevel.js
@@ -1,3 +1,15 @@
+var key_W;
+var key_A;
+var key_S;
+var key_D;
+var key_Space;
+
+var key_Q;
+var key_E;
+var key_Shift;
+
+var speed = 100;
+var character;
 export default class testlevel extends Phaser.Scene {
   constructor(){
     super({key: "testlevel"});
@@ -7,8 +19,32 @@ export default class testlevel extends Phaser.Scene {
     this.load.image('character1', 'character1.png')
 }
 	create(){
-    this.add.image(50, 50, 'character1')
+//keys
+    key_W = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+    key_A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    key_D = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    key_S = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+    key_Space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    key_Q = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
+    key_E = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
+    key_Shift = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT);
+    character = this.physics.add.image(50, 50, 'character1')
 	}
   update(){
+    if (key_D.isDown) {
+      character.setVelocityX(speed);
+    }
+    else if (key_A.isDown) {
+      character.setVelocityX(-speed);
+    }
+    else {
+      character.setVelocityX(0);
+    }
+    if (key_Space.isDown) {
+      character.setVelocityY(-240);
+    }
+    else if(key_W.isDown){
+      character.setVelocityY(-240);
+    }
   }
 }
